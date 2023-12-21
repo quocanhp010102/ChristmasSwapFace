265cf6fa4de575a97273193712410cdd42a2225e
quocanhppp <quocanhppp@quocanhppps-MacBook-Pro.local>
2023-12-21 09:51:21 +0700

    quocanh1

---
+++ ChristmasSwapFace/ViewController/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  ChristmasSwapFace
+//
+//  Created by quocanhppp on 20/12/2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet weak var buttonback:UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        buttonback.setTitle("", for: .normal)
+    }
+
+
+}
+
